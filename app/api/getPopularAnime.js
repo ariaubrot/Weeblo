815eb35b684d0f4a0388476dd05a214c6841f78f
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-22 20:54:06 +0700

    Update getPopularAnime.js

--- app/api/getPopularAnime.js
+++ app/api/getPopularAnime.js
@@ -2,7 +2,7 @@
 export const getPopularAnime = async () => {
     
     const resp = await fetch(
-      `https://kitsu.app/api/edge/trending/anime`,
+      `https://kitsu.app/api/edge/anime`,
       {
         next: {
           revalidate: 60 * 60 * 24,
