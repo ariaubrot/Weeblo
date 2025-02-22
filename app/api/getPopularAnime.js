7932037fb83e649c05d48f712eade16966bf8d99
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-23 01:35:26 +0700

    Update getPopularAnime.js

--- app/api/getPopularAnime.js
+++ app/api/getPopularAnime.js
@@ -1,14 +1,20 @@
 "use server"
 export const getPopularAnime = async () => {
-    
-    const resp = await fetch(
-      `https://wajik-anime-api.vercel.app/samehadaku/popular`,
-      {
-        next: {
-          revalidate: 60 * 60 * 24,
-        },
-      }
-    );
-    const data = await resp.json();
-    return data.data;
-  };
+import axios from "axios";
+
+// Using the example query "demon", and looking at the first page of results.
+const url = "https://api.consumet.org/meta/anilist/trending";
+const data = async () => {
+ try {
+ const { data } = await axios.get(url, { params: {
+ page: 1,
+ perPage: 20
+ } });
+ return data;
+ } catch (err) {
+ throw new Error(err.message);
+ }
+};
+
+console.log(data);
+
